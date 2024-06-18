85d5216730f9ea3d5872200204f436bb27eaf3da
Alice Zvyagina <alicezvyagina@MacBook-Air-Alice.local>
2024-06-18 14:04:58 +0300

    add print in BotConfigPropertiesTest

--- src/test/java/com/telegram_bot/Zviagina_telegram_bot/config/BotConfigPropertiesTest.java
+++ src/test/java/com/telegram_bot/Zviagina_telegram_bot/config/BotConfigPropertiesTest.java
@@ -1,24 +1,23 @@
 package com.telegram_bot.Zviagina_telegram_bot.config;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.TestPropertySource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @TestPropertySource(properties = {"bot.token=secretToken", "bot.name=TestBotName"})
 public class BotConfigPropertiesTest {
 
-    @MockBean
+    @Autowired
     private BotConfig botConfig;
 
     @Test
     public void propertiesAreSet() {
-        when(botConfig.getToken()).thenReturn("secretToken");
-        when(botConfig.getName()).thenReturn("TestBotName");
+        System.out.println("Token: " + botConfig.getToken());
+        System.out.println("Name: " + botConfig.getName());
 
         assertEquals("secretToken", botConfig.getToken());
         assertEquals("TestBotName", botConfig.getName());
