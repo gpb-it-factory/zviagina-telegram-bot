b78c43634576168aa9d350f3e6f6f0ac09645049
Alice Zvyagina <alicezvyagina@MacBook-Air-Alice.local>
2024-06-18 13:26:19 +0300

    reduct BotConfigPropertiesTest

--- src/test/java/com/telegram_bot/Zviagina_telegram_bot/config/BotConfigPropertiesTest.java
+++ src/test/java/com/telegram_bot/Zviagina_telegram_bot/config/BotConfigPropertiesTest.java
@@ -1,22 +1,26 @@
-//package com.telegram_bot.Zviagina_telegram_bot.config;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.TestPropertySource;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//@SpringBootTest
-//@TestPropertySource(properties = {"bot.token=secretToken", "bot.name=TestBotName"})
-//public class BotConfigPropertiesTest {
-//
-//    @Autowired
-//    private BotConfig botConfig;
-//
-//    @Test
-//    public void propertiesAreSet() {
-//        assertEquals("secretToken", botConfig.getToken());
-//        assertEquals("TestBotName", botConfig.getName());
-//    }
-//}
+package com.telegram_bot.Zviagina_telegram_bot.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+@TestPropertySource(properties = {"bot.token=secretToken", "bot.name=TestBotName"})
+public class BotConfigPropertiesTest {
+
+    @MockBean
+    private BotConfig botConfig;
+
+    @Test
+    public void propertiesAreSet() {
+        when(botConfig.getToken()).thenReturn("secretToken");
+        when(botConfig.getName()).thenReturn("TestBotName");
+
+        assertEquals("secretToken", botConfig.getToken());
+        assertEquals("TestBotName", botConfig.getName());
+    }
+}
